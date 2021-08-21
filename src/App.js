ee5a92b8e914c1661ca5e039c5add362c269ad10
kendrickPC <kennethpchang@gmail.com>
2021-08-21 16:51:50 -0700

    Article component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Article } from './components/Article';
+
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev')
+
+  useEffect( () => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if (res.status !== 200)  {
+        console.log("ERROR!");
+        return;
+      }
+      res.json().then(data => {
+        if (data !== null) {
+          // console.log(data);
+          setArticles(data.data.children);
+        }
+      })
+    })
+  }, [subreddit])
+
   return (
     <div className="App">
       <header className="App-header">
-        HEADER
+        <input type="text" className="input" value="webdev" />
       </header>
+      <div className="articles">
+        <Article />
+      </div>
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Article() {
+  return (
+    <article>
+      <a href="#">
+        <h3>Title</h3>
+      </a>
+    </article>
+
+    
+  )
+}
+
+// export default Article;
